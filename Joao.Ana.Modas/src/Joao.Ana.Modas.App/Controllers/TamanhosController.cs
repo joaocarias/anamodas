1dfaa884da60e31bb28046026e0359cb8d652a08
João Carias de França <joaocariasdefranca@gmail.com>
2023-04-15 15:50:42 -0300

    correcao de bug de atualizacao de tamanho

--- Joao.Ana.Modas/src/Joao.Ana.Modas.App/Controllers/TamanhosController.cs
+++ Joao.Ana.Modas/src/Joao.Ana.Modas.App/Controllers/TamanhosController.cs
@@ -121,7 +121,7 @@ namespace Joao.Ana.Modas.App.Controllers
                 if (c is null) return View(model);
 
                 _ = Guid.TryParse(GetUserId(), out Guid userId);
-                c.Atualizar(model.Nome, userId);
+                c.Atualizar(model.Nome, model.Ordem, userId);
 
                 await _tamanhoRepositorio.AtualizarAsync(c);
                 return RedirectToAction(nameof(Detalhar), new { guid = c.Id });
